12ac71afb2d8ea0c81098a8e88306fb82cefa44c
petersowa <petersowa@users.noreply.github.com>
2019-01-09 11:36:30 -0600

    Added initial data to render of TaskApp.

--- src/index.js
+++ src/index.js
@@ -1,9 +1,25 @@
-import React from 'react';
+import React, { Component } from 'react';
 import ReactDOM from 'react-dom';
 
 import * as serviceWorker from './serviceWorker';
 
-const App = () => "Hello World";
+import initialData from './data/initial-data';
+console.log(initialData);
+
+const App = () => 'Hello World';
+
+class TaskApp extends Component {
+  state = initialData;
+
+  render() {
+    return this.state.columnOrder.map(columnId => {
+      const column = this.state.columns[columnId];
+      const tasks = column.taskIds.map(taskId => this.state.tasks[taskId]);
+      return column.title;
+    });
+  }
+}
+
 ReactDOM.render(<App />, document.getElementById('root'));
 
 // If you want your app to work offline and load faster, you can change
